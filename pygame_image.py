d2fd034bef4ca9d913afc77c23282f534b830492
Futa <c0a2305018@edu.teu.ac.jp>
2024-04-16 16:45:39 +0900

    練習７

--- pygame_image.py
+++ pygame_image.py
@@ -11,15 +11,21 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")  #練習２
-    kk_img = pg.transform.flip(kk_img, True , False)  #練習２
+    kk_img = pg.transform.flip(kk_img, True, False)  #練習２
+    
+    bg_img2 = pg.transform.flip(bg_img, True, False)  #練習７ー１
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800  #練習６
+        x = tmr%3200  
         # print(tmr, x)
         screen.blit(bg_img, [-x , 0]) #練習６
+        screen.blit(bg_img2, [-x+1600, 0]) #練習７－1
+        
+        screen.blit(bg_img, [-x+3600, 0]) #練習７－２
+        screen.blit(bg_img2, [-x+4800, 0]) #練習７ー２
         screen.blit(kk_img, [300, 200])  #練習４
         pg.display.update()
         tmr += 1        
